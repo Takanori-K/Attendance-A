aa657684ee3d2e61b32bf1f3854fc6fcd9e69d8f
Kaku <kaku.takanori.1026@gmail.com>
2019-09-21 07:20:18 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucsses] = "新規作成しました。"
+      flash[:success] = "新規作成しました。"
       redirect_to @user
     else
       render :new
