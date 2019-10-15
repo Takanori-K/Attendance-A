6cd21ae525d514726ec3bf7c2ea62f4190efd994
Kaku <kaku.takanori.1026@gmail.com>
2019-10-15 15:06:03 +0000

    残業申請カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191011151329) do
+ActiveRecord::Schema.define(version: 20191015150438) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20191011151329) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "scheduled_end_time"
+    t.string "next_day"
+    t.string "business_description"
+    t.string "instructir_sign"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
