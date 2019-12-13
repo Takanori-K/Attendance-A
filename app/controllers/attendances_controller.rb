14212bdec531a3387d823fb4c5761b0ffb9ec9a8
Kaku <kaku.takanori.1026@gmail.com>
2019-12-13 19:55:42 +0000

    started_finished_change_request_途中

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -11,13 +11,17 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
-      if @attendance.update_attributes(started_at: Time.current.change(sec: 0), applying_started_at: Time.current.change(sec: 0))
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0),
+                                       applying_started_at: Time.current.change(sec: 0),
+                                       approval_started: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end
     elsif @attendance.finished_at.nil?
-      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0), applying_finished_at: Time.current.change(sec: 0))
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0),
+                                       applying_finished_at: Time.current.change(sec: 0),
+                                       approval_finished: Time.current.change(sec: 0))
         flash[:info] = "お疲れ様でした。"
       else
         flash[:danger] = UPDATE_ERROR_MSG
@@ -139,7 +143,7 @@ class AttendancesController < ApplicationController
   private
     
     def attendances_params
-      params.require(:user).permit(attendances: [:applying_started_at, :applying_finished_at, :note, :next_day, :worked_request_sign, :worked_status, :worked_change])[:attendances]
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :applying_started_at, :applying_finished_at, :note, :next_day, :worked_request_sign, :worked_status, :worked_change])[:attendances]
     end
     
     def overtime_params
