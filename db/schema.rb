68bd6aafd86df7d582845fc6a6c9dd3442146186
Kaku <kaku.takanori.1026@gmail.com>
2020-01-18 02:55:32 +0000

    approval_date_column追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191221135603) do
+ActiveRecord::Schema.define(version: 20200118025346) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20191221135603) do
     t.datetime "approval_finished"
     t.datetime "denial_started"
     t.datetime "denial_finished"
+    t.datetime "approval_date"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
