8932ac0958aa382b9c37c64dd21709615ed5ca78
Kaku <kaku.takanori.1026@gmail.com>
2020-01-18 02:02:02 +0000

    attendance-log-表示完了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,9 @@
 class AttendancesController < ApplicationController
   
-  before_action :set_user,       only: [:edit_one_month, :update_one_month, :edit_overtime_work, :update_overtime_work, :update_one_month_info]
+  before_action :set_user,       only: [:edit_one_month, :update_one_month, :edit_overtime_work, :update_overtime_work, :update_one_month_info, :worked_log]
   before_action :logged_in_user, only: [:update, :edit_one_month, :edit_overtime_work, :update_overtime_work]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_mnth]
-  before_action :set_one_month,  only: [:edit_one_month, :update_one_month_info]
+  before_action :set_one_month,  only: [:edit_one_month, :update_one_month_info, :worked_log]
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
@@ -46,6 +46,9 @@ class AttendancesController < ApplicationController
   rescue ActiveRecord::RecordInvalid
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+  
+  def worked_log
   end
   
   def edit_overtime_work
