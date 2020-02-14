867cf0ec4ba3fce7edbd27e1aa7d4a2a2fd51b52
Kaku <kaku.takanori.1026@gmail.com>
2020-02-14 19:45:07 +0000

    勤怠ログ未完了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -53,10 +53,10 @@ class AttendancesController < ApplicationController
   def worked_log
     @all_worked_on = Attendance.where(user_id: current_user.id)
     if params[:search].present?
-      @attendances_search = @all_worked_on.where('worked_on LIKE ?', "%#{params[:search]}%").order(:worked_on)
+     @attendances_search = @all_worked_on.where('worked_on LIKE ?', "#{params[:search]}").order(:worked_on)
     else
       @attendances_search = @all_worked_on.where(worked_on: @first_day..@last_day).order(:worked_on)
-    end  
+    end
   end
   
   def edit_overtime_work
